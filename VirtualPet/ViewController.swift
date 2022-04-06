22d7eb450b45a6bcffff7435e7fdbc418cfa2f6f
Awesa <awesaatyanta.p@gmail.com>
2022-04-06 10:32:34 +0700

    Initial Commit

---
+++ VirtualPet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualPet
+//
+//  Created by Atyanta Awesa Pambharu on 06/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

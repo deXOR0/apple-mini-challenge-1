b3e64ccdf1da83ca5917577dca0a430f2402162c
gonszalito <andryanajona@gmail.com>
2022-04-11 10:20:34 +0700

    Added Journal Model

---
+++ VirtualPet/Journal.swift
@@ -0,0 +1,16 @@
+//
+//  Journal.swift
+//  VirtualPet
+//
+//  Created by Jonathan Andryana on 10/04/22.
+//
+import Foundation
+import UIKit
+
+struct Journal {
+    let journalID: String
+    let dateTime : Date
+    let feelings : Float
+    let gamesPlayed : String
+    let gamingExperience : String
+}

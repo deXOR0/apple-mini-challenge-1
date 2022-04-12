8a87c797dfdc85255e83161c2dcba403408a834f
gonszalito <andryanajona@gmail.com>
2022-04-12 09:30:54 +0700

    Added Journal Page

--- VirtualPet/ViewController.swift
+++ VirtualPet/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var emoji: UILabel!
+    
+    @IBAction func emojiValue(_ sender: UISlider) {
+        let currValue = Float(sender.value)
+        
+        emoji.text = currValue <= 0.2 ? "😞" : currValue <= 0.4 ? "🙁" : currValue <= 0.6 ? "😐" : currValue <= 0.8 ? "🙂"  : "😆"
 
+    }
+    
+    @IBAction func continueButtonPressed(_ sender: Any) {
+    }
 }
 

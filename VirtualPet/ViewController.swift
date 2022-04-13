452e509b8d014b2903a55d41f132bb1a014dbb02
Sherary <sherary.apriliana@gmail.com>
2022-04-13 21:49:57 +0700

    add view for main storyboard

--- VirtualPet/ViewController.swift
+++ VirtualPet/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var affectionMeter: UIProgressView!
+    @IBOutlet weak var hungerMeter: UIProgressView!
+    @IBOutlet weak var money: UITextView!
+    
+     
+    @IBOutlet weak var user: UIImageView!
+    @IBAction func timerSetter(_ sender: Any, forEvent event: UIEvent) {
+    }
+    @IBAction func inventory(_ sender: UIButton){
+        
+    }
+    @IBAction func shop(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
